ee34e47598b7fceca7921960d5228bb04451bfb1
Nam Huynh <namhq1989@users.noreply.github.com>
2022-03-18 11:28:49 +0700

    add pull sub and consumer
    * add pull subscribe
    
    * fix pullsub channel name
    
    * fix pullsub channel name
    
    * add check consumer
    
    * add check consumer
    
    * add check consumer
    
    * add check consumer
    
    * add check consumer
    
    * add check consumer
    
    * add check consumer
    
    * add check consumer
    
    * done add pull sub

--- jetstream_pubsub.go
+++ jetstream_pubsub.go
@@ -13,7 +13,7 @@ func (js JetStream) Publish(stream, subject string, payload []byte) error {
 
 	_, err := js.instance.PublishAsync(channel, payload)
 	if err != nil {
-		msg := fmt.Sprintf("[NATS JETSTREAM] - publish message to subject %s error: %s", channel, err.Error())
+		msg := fmt.Sprintf("[NATS JETSTREAM] - publish message to subject #%s error: %s", channel, err.Error())
 		return errors.New(msg)
 	}
 	return nil
@@ -28,5 +28,38 @@ func (js JetStream) Subscribe(stream, subject string, cb nats.MsgHandler) (*nats
 		msg := fmt.Sprintf("[NATS JETSTREAM] - subscribe subject %s error: %s", channel, err.Error())
 		return nil, errors.New(msg)
 	}
+	return sub, nil
+}
+
+// PullSubscribe ...
+//
+// Example:
+//
+// js := natsio.GetJetStream()
+//
+// sub, err := js.PullSubscribe("A_SUBJECT", "A_SUBJECT", "A_CONSUMER")
+//
+// for {
+//   messages, err := sub.Fetch(10)
+//   // process each messages
+// }
+//
+func (js JetStream) PullSubscribe(stream, subject, consumer string) (*nats.Subscription, error) {
+	channel := combineStreamAndSubjectName(stream, subject)
+
+	// Check if consumer existed
+	con, err := js.GetConsumerInfo(stream, consumer)
+	if con == nil || err != nil {
+		msg := fmt.Sprintf("[NATS JETSTREAM] - pull subscribe consumer %s not existed in stream %s", consumer, stream)
+		return nil, errors.New(msg)
+	}
+
+	// Pull
+	sub, err := js.instance.PullSubscribe(channel, consumer)
+	if err != nil {
+		msg := fmt.Sprintf("[NATS JETSTREAM] - pull subscribe subject #%s - consumer #%s error: %s", channel, consumer, err.Error())
+		return nil, errors.New(msg)
+	}
+
 	return sub, nil
 }
