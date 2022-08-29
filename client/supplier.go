8d62dc710d493a1ff0223d7fac87ae362cc1f8e4
phuanbui <phuanbui@selly.vn>
2022-08-29 14:25:12 +0700

    fix get supplier

--- client/supplier.go
+++ client/supplier.go
@@ -17,8 +17,8 @@ func GetSupplier() Supplier {
 	return Supplier{}
 }
 
-func (s Supplier) GetSupplierInfo(supplierID model.GetSupplierRequest) (*model.ResponseSupplierInfo, error) {
-	msg, err := natsio.GetServer().Request(subject.Supplier.GetSupplierInfo, toBytes(supplierID))
+func (s Supplier) GetListSupplierInfo(p model.GetSupplierRequest) (*model.ResponseSupplierInfo, error) {
+	msg, err := natsio.GetServer().Request(subject.Supplier.GetListSupplierInfo, toBytes(p))
 	if err != nil {
 		return nil, err
 	}
@@ -26,6 +26,27 @@ func (s Supplier) GetSupplierInfo(supplierID model.GetSupplierRequest) (*model.R
 	var r struct {
 		Data  *model.ResponseSupplierInfo `json:"data"`
 		Error string                      `json:"error"`
+	}
+
+	if err := json.Unmarshal(msg.Data, &r); err != nil {
+		return nil, err
+	}
+	if r.Error != "" {
+		return nil, errors.New(r.Error)
+	}
+
+	return r.Data, nil
+}
+
+func (s Supplier) GetSupplierContractBySupplierID(p model.GetSupplierContractRequest) (*model.ResponseSupplierContract, error) {
+	msg, err := natsio.GetServer().Request(subject.Supplier.GetSupplierContractBySupplierID, toBytes(p))
+	if err != nil {
+		return nil, err
+	}
+
+	var r struct {
+		Data  *model.ResponseSupplierContract `json:"data"`
+		Error string                          `json:"error"`
 	}
 
 	if err := json.Unmarshal(msg.Data, &r); err != nil {
