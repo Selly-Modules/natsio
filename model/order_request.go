af6e0843b90cdc5b4985837d90d8e62246f0ff88
Sinh <luuvansinh555@gmail.com>
2022-09-06 11:09:52 +0700

    update order OR model

--- model/order_request.go
+++ model/order_request.go
@@ -2,11 +2,12 @@ package model
 
 // OrderUpdateORStatus ...
 type OrderUpdateORStatus struct {
-	OrderCode      string `json:"orderCode"`
-	ORCode         string `json:"orCode"`
-	Status         string `json:"status"`
-	DeliveryStatus string `json:"deliveryStatus"`
-	Reason         string `json:"reason"`
+	OrderCode      string      `json:"orderCode"`
+	ORCode         string      `json:"orCode"`
+	Status         string      `json:"status"`
+	DeliveryStatus string      `json:"deliveryStatus"`
+	Reason         string      `json:"reason"`
+	Data           OrderORData `json:"data"`
 }
 
 // OrderCancelDelivery ...
@@ -19,4 +20,9 @@ type OrderChangeDeliveryStatus struct {
 	OrderID        string   `json:"orderId"`
 	DeliveryStatus string   `json:"deliveryStatus"`
 	ActionBy       ActionBy `json:"actionBy"`
+}
+
+// OrderORData ...
+type OrderORData struct {
+	Link string `json:"link"`
 }
