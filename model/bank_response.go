0e35b6aec3e79aa43afb81a870753669021bc68a
Tue <nguyenphamquangtue@gmail.com>
2022-09-14 14:25:16 +0700

    bank info

--- model/bank_response.go
+++ model/bank_response.go
@@ -1,6 +1,23 @@
 package model
 
+// MultiLang ...
+type MultiLang struct {
+	En string `json:"en"`
+	Vi string `json:"vi"`
+}
+
 // BankBrief ...
 type BankBrief struct {
-	ID string `json:"_id"`
+	ID                       string    `json:"_id"`
+	Name                     MultiLang `json:"name"`
+	ShortName                string    `json:"shortName"`
+	Active                   bool      `json:"active"`
+	CreatedAt                string    `json:"createdAt"`
+	UpdatedAt                string    `json:"updatedAt"`
+	BenBankName              string    `json:"benBankName"`
+	BankCode                 int       `json:"bankCode"`
+	IsBranchRequired         bool      `json:"isBranchRequired"`
+	SearchString             string    `json:"searchString"`
+	BeneficiaryForVietinbank string    `json:"beneficiaryForVietinbank"`
+	CreatedBy                string    `json:"createdBy,omitempty"`
 }
