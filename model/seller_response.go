35a811886b1113bda8683c1063da558766ae49ac
phuanbui <phuanbui@selly.vn>
2022-09-19 14:52:26 +0700

    edit type id

--- model/seller_response.go
+++ model/seller_response.go
@@ -2,7 +2,7 @@ package model
 
 // ResponseSellerInfo ...
 type ResponseSellerInfo struct {
-	ID   string `json:"id"`
+	ID   string `json:"_id"`
 	Name string `json:"name"`
 	Code string `json:"code"`
 }
