1d58e55af736b32c7526d711161ef9509d6bb83f
trunglt251292 <trunglt251292@gmail.com>
2022-09-20 10:28:35 +0700

    [Update] Request news

---
+++ subject/news.go
@@ -0,0 +1,13 @@
+package subject
+
+import "fmt"
+
+func getNewsValue(val string) string {
+	return fmt.Sprintf("%s.%s", prefixes.Order, val)
+}
+
+var News = struct {
+	GetProductNoticesByInventory string
+}{
+	GetProductNoticesByInventory: getNewsValue("get_product_notices_by_inventory"),
+}
