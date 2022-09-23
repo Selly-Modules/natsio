dcea3798f881cc096b5c6c3e51cad6d186e58ea6
Nguyen Minh <minh.milano1511@gmail.com>
2022-09-23 23:32:23 +0700

    js cashflow seller

--- js/subject/selly.go
+++ js/subject/selly.go
@@ -13,7 +13,9 @@ func getSellyValue(val string) string {
 var Selly = struct {
 	PushNotification               string
 	UpdateSellerAffiliateStatistic string
+	CheckAnDInsertCashflowBySeller string
 }{
 	PushNotification:               getSellyValue("push_notifications"),
 	UpdateSellerAffiliateStatistic: getSellyValue("update_seller_affiliate_statistic"),
+	CheckAnDInsertCashflowBySeller: getSellyValue("check_and_insert_cashflow_statistic"),
 }
